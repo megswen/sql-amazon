bf395ca1993e68e791286f54ef2bd2357c02c3a7
Megan Swenson <mrsmeganswenson@gmail.com>
2020-05-15 15:25:40 -0600

    Started on the Manager app and completed 3 functions. One more to go.

--- bamazonManager.js
+++ bamazonManager.js
@@ -81,12 +81,44 @@ function addInventory(){
         name: "addInvQuant"
     }
     ]).then(function(answer){
-        // START HERE
-
-
-
-
+        connection.query("SELECT * FROM products",
+        function(err, res) {
+            if (err) throw err;
+            //if users enter an item ID outside total items range, then throw error message.
+            if((parseInt(answer.addInvId) > res.length) || (parseInt(answer.addInvId) <= 0)) {
+                console.log("Please enter a valid ID.");
+            }
+            //otherwise, proceed to loop through the data and find matched item id as the selected item
+            var chosenItem = "";
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === parseInt(answer.addInvId)) {
+                chosenItem = res[i];
+                }
+            }
+        
+            connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                    stock_quantity: chosenItem.stock_quantity += parseInt(answer.addInvQuant)
+                    },
+                    {
+                    item_id: chosenItem.item_id
+                    }
+                ],
+                function(error) {
+                    if (error) throw err;
+                    //show message that certain product and its quantity has been updated in the inventory.
+                    console.log("Successfully added "+ answer.addInvQuant + " " + chosenItem.product_name + "(s) to the inventory.");
+                    viewProducts();
+                }
+                );
+        });
     });
+
+
+
+
+    // });
 }
 
 // // Function for Add new product
